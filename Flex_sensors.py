c5fb7783342c7e8b4aa53108008b1a98432be046
Adeland10 <adele.faille@live.fr>
2024-05-03 17:14:42 +0200

    commit flex_sensors ok

---
+++ Flex_sensors.py
@@ -0,0 +1,10 @@
+class Flex_sensors:
+    def __init__(self, finger, pinA, pinB):
+        self.finger = finger
+        self.pinA = pinA
+        self.pinB = pinB
+        self.value = None
+    
+    def analogread(self):
+        print(f"- Finger : {self.finger}\n - Flex : {self.value}")
+
